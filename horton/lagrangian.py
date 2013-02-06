Fixed full gradient. Still need to fix UT gradient.

horton/lagrangian.py
@@ -176,7 +176,8 @@ class lagrangian(object):
             
             fd = (fn(tmpFwd) - fn(tmpBack)) / (np.float64(2)*h)
             an = (anfn(tmpFwd) - anfn(tmpBack)) / (np.float64(2)*h)
-            assert (np.abs(fd - an) < 1e-4).all(), np.abs(fd-an) > 1e-4
+            fdan = np.abs(fd - an)
+            assert (fdan < 1e-6).all(), (fdan > 1e-6, np.where(fdan > 1e-6), fdan)
             
             result.append(fd)
 
@@ -220,39 +221,42 @@ class lagrangian(object):
         
         result = []
     
-#        self.sys.wfn.invalidate()
-#        self.ham.invalidate()
-#        self.sys.wfn.update_dm("alpha", self.toOneBody(self.lf,da)[0])
-#        self.sys.wfn.update_dm("beta", self.toOneBody(self.lf,db)[0])
-#        self.sys.wfn.update_dm("full", self.toOneBody(self.lf,(da+db))[0])
+        self.sys.wfn.invalidate()
+        self.ham.invalidate()
+        self.sys.wfn.update_dm("alpha", self.toOneBody(self.lf,da)[0])
+        self.sys.wfn.update_dm("beta", self.toOneBody(self.lf,db)[0])
+        self.sys.wfn.update_dm("full", self.toOneBody(self.lf,(da+db))[0])
 #            self.sys.wfn.update_dm("spin", self.toOneBody(self.lf,(da-db))[0])
     
-#        self.fock_alpha.reset()
-#        self.fock_beta.reset()
+        self.fock_alpha.reset()
+        self.fock_beta.reset()
         
 #        if self.ham.grid is None:
-#            self.ham.compute_fock(self.fock_alpha, None)
-#            self.ham.compute_fock(self.fock_beta, None) #print "HACKHACKHACK! THIS IS FOR HF!!!!!!"
-#        else:
 #            self.ham.compute_fock(self.fock_alpha, self.fock_beta) #print "HACK HACK HACK! THIS IS FOR DFT!!!!"
+#
+##            self.ham.compute_fock(self.fock_alpha, None)
+##            self.ham.compute_fock(self.fock_beta, None) #print "HACKHACKHACK! THIS IS FOR HF!!!!!!"
+#        else:
+        self.ham.compute_fock(self.fock_alpha, self.fock_beta) #print "HACK HACK HACK! THIS IS FOR DFT!!!!"
+        
+        self.sys.wfn.invalidate()
+        self.sys.wfn.update_exp(self.fock_alpha, self.fock_beta, self.sys.get_overlap(), toOneBody(self.lf,da)[0], toOneBody(self.lf,db)[0])
+        
+        numpy_fock_alpha = toNumpy(self.fock_alpha)
+        numpy_fock_beta = toNumpy(self.fock_beta)
 #        
-#        self.sys.wfn.invalidate()
-#        self.sys.wfn.update_exp(self.fock_alpha, self.fock_beta, self.sys.get_overlap(), toOneBody(self.lf,da)[0], toOneBody(self.lf,db)[0])
-        
-#        numpy_fock_alpha = toNumpy(self.fock_alpha)
-#        numpy_fock_beta = toNumpy(self.fock_beta)
-        
-#        for spins in [[numpy_fock_alpha, da,ba,pa,mua,N], [numpy_fock_beta, db,bb,pb,mub,N2]]:
-        for spins in [[da,ba,pa,mua,N], [db,bb,pb,mub,N2]]:
+        for spins in [[numpy_fock_alpha, da,ba,pa,mua,N], [numpy_fock_beta, db,bb,pb,mub,N2]]:
+#        for spins in [[da,ba,pa,mua,N], [db,bb,pb,mub,N2]]:
 #            dLdD = self.old_grad_fock(da,db)
             
-            [D,B,P,Mu,n] = spins
-#            [fock,D,B,P,Mu,n] = spins
-#            dLdD = fock[0]
-            dLdD = 0
-            wrappedD = toOneBody(self.lf, da, db)
-            dLdD = toNumpy(self.dVee.dD(wrappedD))
-            dLdD += (self.T - self.V)
+#            [D,B,P,Mu,n] = spins
+#            dLdD = 0
+            [fock,D,B,P,Mu,n] = spins
+            dLdD = fock[0]
+            
+#            wrappedD = toOneBody(self.lf, da, db, D)
+#            dLdD = toNumpy(self.dVee.dD(*wrappedD))
+#            dLdD += (self.T - self.V)
             
 #            assert (np.abs(fock - self.old_grad_fock(da, db)) < 1e-5).all(), fock-self.old_grad_fock(da, db)
             
@@ -568,5 +572,8 @@ class lagrangian(object):
     def sym_lin_grad_wrap(self,x):
         self.tri_offsets()
         args = self.UTvecToMat(x)
+        
+        fdan = np.abs(self.fdiff_hess_grad_grad(x) - self.sym_gradient_spin_frac(*args))
+        assert (fdan < 1e-6).all(), (fdan < 1e-6, np.where(fdan > 1e-6), fdan)
         
         return self.sym_gradient_spin_frac(*args)

horton/test/test_newton_rewrite.py
@@ -129,7 +129,7 @@ def promol_guess(sys, basis):
         
         converge_scf_oda(ham)
         
-        orb_alpha.append(atom_sys.wfn.exp_alpha._coeffs) #Why is the sign wrong?
+        orb_alpha.append(atom_sys.wfn.exp_alpha._coeffs)
         orb_beta.append(atom_sys.wfn.exp_beta._coeffs)
         
         occ_alpha.append(atom_sys.wfn.exp_alpha.occupations)
@@ -231,6 +231,27 @@ def test_H2O_DM():
     
 #test_H2O_DM()
 
+def test_grad():
+    basis = 'sto-3g' #CHANGE1
+    system = System.from_file('water_equilim.xyz', obasis=basis)
+    system.init_wfn(charge=0, mult=1, restricted=False)
+    
+    dm_a, dm_b, occ_a, occ_b, energy_a, energy_b = promol_guess(system, basis)
+    [pro_da, pro_ba, pro_db, pro_bb, mua, mub, N, N2] = promol_h2o(dm_a, dm_b, occ_a, occ_b, energy_a, energy_b)
+
+    S = system.get_overlap()._array
+    
+    ham = Hamiltonian(system, [HartreeFock()])
+
+    lf, lg, nbasis, wfn = _lg_init(system, ham, N,N2,[pro_da, pro_db, pro_ba, pro_bb, pro_da, pro_db, mua, mub])
+    
+    pa = sqrtm(np.dot(np.dot(S,pro_da),S) - np.dot(np.dot(np.dot(np.dot(S,pro_da),S),pro_da),S)) #Move to promol_guess()
+    pb = sqrtm(np.dot(np.dot(S,pro_db),S) - np.dot(np.dot(np.dot(np.dot(S,pro_db),S),pro_db),S)) #Move to promol_guess()
+    
+    x0 = np.hstack([pro_da.ravel(), pro_db.ravel(), pro_ba.ravel(), pro_bb.ravel(), pa.ravel(), pb.ravel(), mua, mub]); lg.isUT = False
+
+    assert np.abs(lg.fdiff_hess_grad_grad(x0) - lg.lin_grad_wrap(x0)) < 1e-6
+    
 
 def test_H2O_New():
     solver = NewtonKrylov()
@@ -268,13 +289,13 @@ def test_H2O_New():
 
     lf, lg, nbasis, wfn = _lg_init(system, ham, N,N2,[pro_da, pro_db, pro_ba, pro_bb, pro_da, pro_db, mua, mub])
     
-    pa = sqrtm(np.dot(np.dot(S,pro_da),S) - np.dot(np.dot(np.dot(np.dot(S,pro_da),S),pro_da),S))
-    pb = sqrtm(np.dot(np.dot(S,pro_db),S) - np.dot(np.dot(np.dot(np.dot(S,pro_db),S),pro_db),S))
+    pa = sqrtm(np.dot(np.dot(S,pro_da),S) - np.dot(np.dot(np.dot(np.dot(S,pro_da),S),pro_da),S)) #Move to promol_guess()
+    pb = sqrtm(np.dot(np.dot(S,pro_db),S) - np.dot(np.dot(np.dot(np.dot(S,pro_db),S),pro_db),S)) #Move to promol_guess()
     
     ind = np.triu_indices(dm_a.shape[0])
     
-    x0 = np.hstack([pro_da.ravel(), pro_db.ravel(), pro_ba.ravel(), pro_bb.ravel(), pa.ravel(), pb.ravel(), mua, mub]); lg.isUT = False
-#    x0 = np.hstack([pro_da[ind], pro_db[ind], pro_ba[ind], pro_bb[ind], pa[ind], pb[ind], mua, mub]); lg.isUT = True
+#    x0 = np.hstack([pro_da.ravel(), pro_db.ravel(), pro_ba.ravel(), pro_bb.ravel(), pa.ravel(), pb.ravel(), mua, mub]); lg.isUT = False
+    x0 = np.hstack([pro_da[ind], pro_db[ind], pro_ba[ind], pro_bb[ind], pa[ind], pb[ind], mua, mub]); lg.isUT = True
 #    lg.test_UTconvert(x0)
 
     x_star = solver.solve(lg, x0)