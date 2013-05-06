Added checks for gradient using common.checkDelta and fixed mcWeeny purification/normalization code.

horton/initialGuess.py
@@ -1,6 +1,6 @@
 from horton.newton_rewrite import NewtonKrylov
-from horton.gbasis.cext import GBasis
-from horton import *
+from horton.gbasis.cext import GOBasis
+from horton import System, guess_hamiltonian_core, Hamiltonian, converge_scf_oda, HartreeFock, Hartree, BeckeMolGrid, LibXCLDATerm, context
 import numpy as np
 import scipy as scp
 import matplotlib.pyplot as plt
@@ -154,7 +154,7 @@ def generic_HF_calc(basis = 'sto-3g', filename='test/water_equilim.xyz'):
     system = System.from_file(context.get_fn(filename), obasis=basis)
     system.init_wfn(charge=0, mult=1, restricted=False)
     ham = Hamiltonian(system, [HartreeFock()])
-    return system, ham
+    return system, ham, basis
     
 def generic_DFT_calc(basis = 'sto-3g', filename='test/water_equilim.xyz', lda_term = 'c_vwn'):
     system = System.from_file(context.get_fn(filename), obasis=basis)
@@ -162,7 +162,7 @@ def generic_DFT_calc(basis = 'sto-3g', filename='test/water_equilim.xyz', lda_te
     grid = BeckeMolGrid(system, random_rotate=False)
     libxc_term = LibXCLDATerm(lda_term) #vwn_5
     ham = Hamiltonian(system, [Hartree(), libxc_term], grid)
-    return system, ham
+    return system, ham, basis
 
 def calc_shapes(*args):
     shapes = []
@@ -174,7 +174,7 @@ def calc_shapes(*args):
     return shapes
 
 def project(orig_sys, proj_sys, *args):
-    new_basis = horton.gbasis.cext.GOBasis.concatenate(orig_sys.obasis, proj_sys.obasis)
+    new_basis = GOBasis.concatenate(orig_sys.obasis, proj_sys.obasis)
     mixed_sys = System(orig_sys.coordinates, orig_sys.numbers, obasis = new_basis)
     mixed_S = mixed_sys.get_overlap()._array 
     orig_S = orig_sys.get_overlap()._array
@@ -192,18 +192,16 @@ def project(orig_sys, proj_sys, *args):
         result.append(reduce(np.dot,[proj_inv_S,rect_S.T,i,rect_S,proj_inv_S]))
     return result
 
-def normalize_D(*args):
-    result = []
-    for i in args:
-        n = np.sum(i)
-        print "initial normalization: " + str(n)
-        result.append(i/n)
-        assert result[-1].sum()-1 < 1e-8
+def normalize_D(sys, D, N):
+    S = sys.get_overlap()._array
+    n = np.dot(S.ravel(), D.ravel())
+    print "initial normalization: " + str(n)
+    result = D*N/n
     return result
 
-def mcPurify(*args):
+def mcPurify(sys, *args):
     result = []
     for i in args:
         assert isinstance(i,np.ndarray)
-        result.append(3*np.power(i,2) - 2*np.power(i,3))
+        result.append(3*np.power(np.dot(sys.get_overlap()._array, i),2) - 2*np.power(np.dot(sys.get_overlap()._array, i),3))
     return result

horton/test/test_initialGuess.py
@@ -3,6 +3,7 @@ from horton import *
 import numpy as np
 import scipy as scp
 import matplotlib.pyplot as plt
+from horton.guess import guess_hamiltonian_core
 
 def test_projection():
     xyzFile = context.get_fn('test/water_equilim.xyz')
@@ -28,11 +29,35 @@ def test_projection():
     
     for i,j in zip(old_occ, new_occ):
         assert np.abs(i-j) < 1e-2, (i,j)
-        
-#     for i,j in ((pro_da,result[0]),(pro_db, result[1])):
-#         ni = np.linalg.norm(i)
-#         nj = np.linalg.norm(j)
-#         assert np.abs(ni-nj)<1e-2, (ni,nj)
 
+def test_mcPurify():
+    system, ham, basis = initialGuess.generic_DFT_calc()
+    S = system.get_overlap()._array
+    guess_hamiltonian_core(system)
+    D = system.wfn.dm_alpha._array
+    
+    Eigval_before = np.linalg.eigvalsh(np.dot(S,D))
+    avgEigval_before = np.average(np.abs(Eigval_before -0.5))
+    
+    D = initialGuess.mcPurify(system, D)[0]
+    
+    Eigval_after = np.linalg.eigvalsh(np.dot(S,D))
+    avgEigval_after = np.average(np.abs(Eigval_after -0.5))
+    
+    assert avgEigval_after > avgEigval_before
+
+def test_normalize_D():
+    system, ham, basis = initialGuess.generic_DFT_calc()
+    S = system.get_overlap()._array
+    guess_hamiltonian_core(system)
+    D = system.wfn.dm_alpha._array
+    
+    newD = normalize_D(system, D, 5)
+    
+    N = np.dot(system.get_overlap()._array.ravel(), newD.ravel())
+    assert np.abs(N - 5) < 1e-8
+
+# test_normalize_D()
+# test_mcPurify()
 # test_projection()
 

horton/test/test_newton_rewrite.py
@@ -1,8 +1,31 @@
 from horton import *
 import numpy as np
 import matplotlib.pyplot as plt
+from horton.test import common
 
 np.set_printoptions(threshold = 2000)
+
+def test_check_grad():
+    system, ham, basis = initialGuess.generic_DFT_calc()
+    S = system.get_overlap()._array
+
+    dm_a, dm_b, occ_a, occ_b, energy_a, energy_b, nbasis = initialGuess.promol_orbitals(system, basis)
+    occ_a = np.array([1,1,2/3.,2/3.,2/3.,0.5,0.5]); N=5 #STO-3G ONLY
+    occ_b = np.array([1,1,2/3.,2/3.,2/3.,0.5,0.5]); N2=5 #STO-3G ONLY
+    pro_da, pro_ba, pro_db, pro_bb, mua, mub, N, N2 = initialGuess.promol_guess(dm_a, dm_b, occ_a, occ_b, energy_a, energy_b, N, N2)
+    pa, pb = initialGuess.promol_frac(system, pro_da, pro_db)
+
+    args = [pro_da, pro_ba, pa, pro_db, pro_bb, pb, mua, mub]
+    
+    norm_a = LinearConstraint(system, N, np.eye(dm_a.shape[0]), select="alpha")
+    norm_b = LinearConstraint(system, N2, np.eye(dm_a.shape[0]), select="beta")
+    lg = Lagrangian(system, ham, [norm_a, norm_b], isFrac = True)
+    
+    x0 = initialGuess.prep_D(lg, *args)
+    
+    dxs = [np.random.uniform(-0.5, 0.5, len(x0)) for i in np.arange(100)]
+    
+    common.check_delta(lg.lagrange_wrap, lg.grad_wrap, x0, dxs)
 
 def calc_H2O():
     solver = NewtonKrylov()
@@ -624,8 +647,7 @@ def test_linear_stepped_constraints():
 
 def test_quadratic_stepped_constraints():
     solver = NewtonKrylov()
-    basis = "sto-3g"
-    sys, ham = initialGuess.generic_DFT_calc(basis = basis, lda_term = "c_vwn")
+    sys, ham, basis = initialGuess.generic_DFT_calc(lda_term = "c_vwn")
     
     dm_a, dm_b, occ_a, occ_b, energy_a, energy_b, nbasis = initialGuess.promol_orbitals(sys, basis, ifCheat = True)
     occ_a = np.array([1,1,2/3.,2/3.,2/3.,0.5,0.5]); N=5 #STO-3G ONLY
@@ -649,8 +671,9 @@ def test_quadratic_stepped_constraints():
     print "Computed E:" + str(ham.compute_energy())
     
   
+test_check_grad()
 # test_linear_stepped_constraints()
-test_quadratic_stepped_constraints()
+# test_quadratic_stepped_constraints()
 # # calc_H2O()
 # # Horton_H2O()
 # test_HF_STO3G()